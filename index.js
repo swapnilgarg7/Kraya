71515ea1b6d1b9b33fc4801f1aec0686025832ba
Adarsh Jena <76236137+adarshnjena@users.noreply.github.com>
2023-01-30 03:31:55 -0800

    faq section js added

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ const toggleNavbar = () => {
 };
 
 menuToggleButton.addEventListener("click", () => toggleNavbar());
+
+//faq section
+
+const faqToggleButton = document.querySelectorAll(".faqToggleButton");
+const closeAllAnswers = document.querySelectorAll(".mobfaqs");
+
+faqToggleButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    closeAllAnswers.forEach((answer) => {
+      answer.classList.remove("queClicked");
+    });
+    faqToggleButton.forEach((button) => {
+      button.classList.remove("bx-x");
+      button.classList.add("bx-plus");
+    });
+    if (button.classList.contains("bx-plus")) {
+      button.classList.remove("bx-plus");
+      button.classList.add("bx-x");
+      button.parentElement.parentElement.classList.toggle("queClicked");
+    } else {
+      button.classList.remove("bx-x");
+      button.classList.add("bx-plus");
+    }
+  });
+});
